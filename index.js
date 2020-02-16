278bc77ec7aacde1ba91f18b80d6253b850c67dd
Kwon770 <sckwon770@gmail.com>
2020-02-17 07:52:17 +0900

    2-2 2-3 DOM Function

--- index.js
+++ index.js
@@ -1,57 +1,7 @@
-// alert("Im Working");
-// console.log("Im Working");
+// DOM : Document Object Module
+const title = document.getElementById("title");
+const title = document.querySelector("#title");
+title.innerHTML = "No works!";
+title.style.color = "red";
 
-// let : Declare changable variable
-// const : Declare unchangable variable
-// var : Declare changable variable [ old version variable type] [ Not recommended]
-let a = 221;
-let b = a - 5;
-const c = 5;
-var d = 10;
-a = 4;
-
-// Use const as variable type by default
-// Dont use let if you don't have to change variable value
-const bool = true;
-const string = "This is string";
-const integral = 100;
-const float = 100.1;
-
-// Array
-const something = "something";
-const array = ["Mon", "Tue", "Wed", true, 99, something];
-console.log(array[2]);
-
-// Objecct
-const Object = {
-  name: "Nico",
-  age: 33,
-  isHandsome: true,
-  favMovies: ["batman", "Ironman"],
-  favFood: [
-    { name: "Kimchi", fatty: false },
-    { name: "Burger", fatty: true }
-  ]
-};
-console.log(Object.name); // "Nico"
-Object.name = "Inkyung";
-console.log(Object.name); // "Inkyung"
-
-console.log(Object.favFood[0].name);
-
-// Function
-function SayHello(anyName, anyNumber) {
-  console.log("Hello" + anyName + "and" + anyNumber);
-  console.log(`Hello" ${anyName}  "and" ${anyNumber}`);
-}
-
-const greeting = SayHello("You", 15);
-console.log(greeting);
-
-const calculator = {
-  puls: function(a, b) {
-    return a + b;
-  }
-};
-const plus = calculator.puls(5, 5);
-console.log(plus);
+document.title = "I'm here";
