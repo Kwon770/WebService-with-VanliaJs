552931f08cc2fa1ee5c76e1be063bb0e3dd93e2a
Kwon770 <sckwon770@gmail.com>
2020-02-19 08:23:15 +0900

    3-3 Saving the user name part one

---
+++ Web Pratice/greeting.js
@@ -0,0 +1,25 @@
+const form = document.querySelector(".js-form");
+const input = form.querySelector("input");
+const greeting = document.querySelector(".js-greetings");
+
+const USER_LS = "currentUser";
+const SHOWING_CN = "showing";
+
+function PaingGreeting(text) {
+  form.classList.remove(SHOWING_CN);
+  greeting.classList.add(SHOWING_CN);
+  greeting.innerText = `Hello ${text}`;
+}
+
+function LoadName() {
+  const currentUser = localStorage.getItem(USER_LS);
+  if (currentUser === null) {
+  } else {
+    PaingGreeting(currentUser);
+  }
+}
+
+function init() {
+  LoadName();
+}
+init();
