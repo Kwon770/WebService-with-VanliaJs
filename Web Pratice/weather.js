fd3fa7df2fcd61f7ee427e34d15374c45da1ffdb
Kwon770 <sckwon770@gmail.com>
2020-02-20 08:11:56 +0900

    3-10 Getting the weather part two (api)

--- Web Pratice/weather.js
+++ Web Pratice/weather.js
@@ -1,6 +1,31 @@
+const API_KEY = "062aa5e38df65a32830c83c7940b0e0f";
 const COORDS = "coords";
 
-function saveCoords(coordsObj) {
+const weather = document.querySelector(".js-weather");
+
+function GetWeather(lat, lon) {
+  // fetch(``) : 데이터를 가져옴.  ('')(따옴표아닌) (``)(backtick 사용)
+  // https:// 붙여야함
+  // Api 요청에 파라미터 추가는 & 를 이용
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+  )
+    .then(function(response) {
+      // then() 앞의 함수가 완전히 이행된 후 전달 받은 함수를 실행
+      // fetch() 는 네트워크를 통하 험수이므로, then() 없이 코딩할시 fetch가 완전히 이루어지기 전에 코드가 진행
+
+      return response.json();
+    })
+    .then(function(json) {
+      // then() 함수는 계속 연장될 수 있음. .then(~).then(~);
+
+      const temperature = json.main.temp;
+      const place = json.name;
+      weather.innerHTML = `${temperature} @ ${place}`;
+    });
+}
+
+function SaveCoords(coordsObj) {
   localStorage.setItem(COORDS, JSON.stringify(coordsObj));
 }
 
@@ -13,7 +38,7 @@ function HandleGeoSuccess(position) {
     latitude,
     longitude
   };
-  saveCoords(coordsObj);
+  SaveCoords(coordsObj);
 }
 
 function HandleGeoError() {
@@ -25,11 +50,12 @@ function AskForCoords() {
 }
 
 function LoadCoords() {
-  const loadedCords = localStorage.getItem(COORDS);
-  if (loadedCords === null) {
+  const loadedCoords = localStorage.getItem(COORDS);
+  if (loadedCoords === null) {
     AskForCoords();
   } else {
-    //
+    const parseCoords = JSON.parse(loadedCoords);
+    GetWeather(parseCoords.latitude, parseCoords.longitude);
   }
 }
 
